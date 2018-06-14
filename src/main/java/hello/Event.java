7fe823d65f740f5381a64da58135942f276dad4f
Hubert Pyzik <hubert.pyzik@schibsted.com>
2018-06-14 17:03:41 +0200

    feat: initial commit with events endpoint

---
+++ src/main/java/hello/Event.java
@@ -0,0 +1,20 @@
+package hello;
+
+public class Event {
+
+    private final String id;
+    private final String name;
+
+    public Event(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/hello/EventController.java
@@ -0,0 +1,25 @@
+package hello;
+
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EventController {
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/events")
+    public Event events(
+            @RequestParam(value = "name", required = true) String name,
+            @RequestParam(value = "id", required = false) Optional<String>id
+    ) {
+        String eventId = id.isPresent()
+                ? id.get()
+                : String.valueOf(counter.incrementAndGet());
+
+        return new Event(eventId, name);
+    }
+}
